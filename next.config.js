dbe458a84e705632b2c96b4721a56d478d9a7efa
José Rômulo <joseromulo.10@hotmail.com>
2023-05-21 13:13:41 -0300

    feat: add enviroment variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        REACT_APP_API_URL: 'https://teste-0sg8.onrender.com'
+    }
+}
 
 module.exports = nextConfig
