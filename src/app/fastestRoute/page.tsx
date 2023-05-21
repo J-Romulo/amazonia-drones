47b2eace4fbe29840ef02a0553dfc80074f11e9d
José Rômulo <joseromulo.10@hotmail.com>
2023-05-21 13:15:57 -0300

    feat: add board to fastestRoute page and calculate

--- src/app/fastestRoute/page.tsx
+++ src/app/fastestRoute/page.tsx
@@ -1,13 +1,69 @@
 'use client'
+import { Chessboard } from '@/components/Chessboard'
 import * as HomeStyle from '../styles'
 import * as S from './styles'
+import ReactHintFactory from 'react-hint';
+import 'react-hint/css/index.css';
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import api from '@/services/api';
+import Loader from '@/components/Loader';
+
+const ReactHint = ReactHintFactory(React);
 
 export default function FastestRoute(){
-    return(
-        <HomeStyle.Container>
-            <S.MainContent>
+    const [isLoading, setIsLoading] = useState(false)
+    const [currentFastestRoute, setCurrentFastestRoute] = useState([])
+    const [currentTotalTime, setCurrentTotalTime] = useState()
 
+    async function calculateFastestRoute(starting_point: string, object: string, destination: string){
+        try{
+            setIsLoading(true)
+            const { data } = await api.post('/drones/path', {starting_point, object, destination})
+
+            setCurrentFastestRoute(data.path)
+            setCurrentTotalTime(data.total_time)
+            setIsLoading(false)
+        }catch(err){
+            console.log(err)
+            toast.error('Ocorreu um erro ao calcular a rota. Tente novamente em instantes.')
+        }
+    }
+
+    return(
+        <>
+        <ToastContainer autoClose={6000} toastStyle={{fontSize: '0.5rem'}}/>
+        <HomeStyle.Container>
+            <ReactHint autoPosition={true} events className='hint react-hint' />
+            <S.MainContent>
+                {isLoading &&
+                    <S.LoadingScreen>
+                        <Loader color='#FF0048'/>
+                    </S.LoadingScreen>
+                }
+                <S.LeftSection>
+                    <Chessboard calculateRoute={calculateFastestRoute}/>
+                </S.LeftSection>
+                <S.RightSection>
+                        <S.Title>Está pronto para calcular sua entrega?</S.Title>
+                        <S.GuidanceText>Posicione os elementos no tabuleiro ao lado para formar sua rota de entrega e clique em "Calcular rota". O resultado irá aparecer logo abaixo.</S.GuidanceText>
+
+                        {currentFastestRoute.length > 0 && currentTotalTime && 
+                            <S.FastestRouteInfo>
+                                <S.FastestRouteField>
+                                    <S.FastestRouteLabel>Essa é a rota mais rapida para sua entrega ser concluída:</S.FastestRouteLabel>
+                                    <S.FastestRouteData>{currentFastestRoute.join(' -> ')}</S.FastestRouteData>
+                                </S.FastestRouteField>
+                                <S.FastestRouteField>
+                                    <S.FastestRouteLabel>Tempo total:</S.FastestRouteLabel>
+                                    <S.FastestRouteData>{currentTotalTime} segundos</S.FastestRouteData>
+                                </S.FastestRouteField>
+                            </S.FastestRouteInfo>
+                        }
+                </S.RightSection>
             </S.MainContent>
         </HomeStyle.Container>
+        </>
     )
 }
--- src/app/fastestRoute/styles.ts
+++ src/app/fastestRoute/styles.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const MainContent = styled.main`
+    position: relative;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     align-items: center;
     padding: 1.5rem;
     row-gap: 1rem;
@@ -13,4 +14,62 @@ export const MainContent = styled.main`
     box-shadow: 0px 13px 27px -5px rgba(50, 50, 93, 0.25),
 		0px 8px 16px -8px rgba(0, 0, 0, 0.3),
 		0px -6px 16px -6px rgba(0, 0, 0, 0.025);
+`
+
+export const RightSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`
+
+export const Title = styled.h2`
+  font-size: 1.5rem;
+  color: #FF0048;
+`
+
+export const GuidanceText = styled.p`
+  color: #ff0048;
+  font-size: 0.9rem;
+  margin-top: 0.7rem;
+`
+export const LeftSection = styled.section`
+  display: flex;
+  justify-content: center;
+  width: 80%;
+  height: 100%;
+`
+
+export const FastestRouteInfo = styled.div`
+  width: 100%;
+`
+
+export const FastestRouteField = styled.div`
+  margin-top: 1.3rem;
+`
+
+export const FastestRouteLabel = styled.p`
+  color: #FF0048;
+  font-weight: bold;
+  font-size: 0.9rem;
+  margin-bottom: 0.5rem;
+`
+
+export const FastestRouteData = styled.span`
+  color: #FF0048;
+  font-size: 0.8rem;
+`
+
+export const LoadingScreen = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  top: 0;
+  left: 0;
+  background-color: rgba(255, 255, 255, 0.5);
 `
