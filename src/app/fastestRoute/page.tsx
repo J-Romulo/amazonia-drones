b89cbf1d87e294dcdbcd8d3423e64a490d326d22
José Rômulo <joseromulo.10@hotmail.com>
2023-05-21 08:51:36 -0300

    feat: create fastest route page base

---
+++ src/app/fastestRoute/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import * as HomeStyle from '../styles'
+import * as S from './styles'
+
+export default function FastestRoute(){
+    return(
+        <HomeStyle.Container>
+            <S.MainContent>
+
+            </S.MainContent>
+        </HomeStyle.Container>
+    )
+}
---
+++ src/app/fastestRoute/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const MainContent = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1.5rem;
+    row-gap: 1rem;
+    width: 75%;
+    height: 80%;
+    border-radius: 10px;
+    background-color: white;
+    box-shadow: 0px 13px 27px -5px rgba(50, 50, 93, 0.25),
+		0px 8px 16px -8px rgba(0, 0, 0, 0.3),
+		0px -6px 16px -6px rgba(0, 0, 0, 0.025);
+`
